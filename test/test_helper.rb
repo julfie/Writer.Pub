6d6583d09e661470d9ce8b5b3026f2daa340dad4
Juliann <jbfields@andrew.cmu.edu>
2017-09-14 18:16:18 -0400

    db migrated

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,22 +1,26 @@
 require 'simplecov'
 SimpleCov.start 'rails'
-ENV["RAILS_ENV"] ||= "test"
+ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require 'turn/autorun'
+require 'minitest/rails'
+require 'minitest/reporters'
 require 'contexts'
+require 'minitest_extensions'
 
 class ActiveSupport::TestCase
-  ActiveRecord::Migration.check_pending!
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  # fixtures :all
+
   # Add more helper methods to be used by all tests here...
-  # start by including the Contexts module
   include Contexts
 
+  # Add the infamous deny method...
   def deny(condition, msg="")
     # a simple transformation to increase readability IMO
     assert !condition, msg
   end
-end
 
-# Formatting test output a litte nicer
-Turn.config.format = :outline
+  # Spruce up minitest results...
+  Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new]
+end
