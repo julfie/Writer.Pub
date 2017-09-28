684a1bf2ec5c63ea354f890970b73d454901ad3d
Juliann <jbfields@andrew.cmu.edu>
2017-09-28 14:59:49 -0400

    user role edits

--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -2,16 +2,11 @@ class UserRole < ActiveRecord::Base
 	
 	
 		# Relationships
-		  belongs_to :user
-		  belongs_to :role
+			belongs_to :user
+			belongs_to :role
 	
-		  # Scopes	  
-		  
-		  # Validations
-	
-		  validates_presence_of :user_id
-		  validates_presence_of :role_id
-		  
-	
-		  # Methods
+			# Validations
+			validates_presence_of :user_id
+			validates_presence_of :role_id
+			
 	end
--- test/sets/user_roles.rb
+++ test/sets/user_roles.rb
@@ -15,35 +15,35 @@ module Contexts
 
     # Context for both customer and employee users
     def create_user_artist_roles
-      @bobby_a    = FactoryGirl.create(:user_role, user: @evan , role: @artist)
-      @obeezy_a   = FactoryGirl.create(:user_role, user: @juliann, role: @artist)
-      @fakey_a    = FactoryGirl.create(:user_role, user: @jim, role: @artist)
-      @lazy_a     = FactoryGirl.create(:user_role, user: @jim, role:  @artist) 
+      @evan_a    = FactoryGirl.create(:user_role, user: @evan , role: @artist)
+      @juli_a   = FactoryGirl.create(:user_role, user: @juliann, role: @artist)
+      @jim_a    = FactoryGirl.create(:user_role, user: @jim, role: @artist)
+      @ev_a     = FactoryGirl.create(:user_role, user: @ev, role:  @artist) 
     end
     
     def destroy_user_artist_roles
-      @bobby_a.delete
-      @obeezy_a.delete
-      @fakey_a.delete
-      @lazy_a.delete 
+      @evan_a.delete
+      @juli_a.delete
+      @jim_a.delete
+      @ev_a.delete 
     end
 
     def create_user_producer_roles
-      @bibby_p = FactoryGirl.create(:user_role, user: @ev, role: @producer)
-      @dean_p   = FactoryGirl.create(:user_role, user: @juliann, role:  @producer)
+      @ev_p = FactoryGirl.create(:user_role, user: @ev, role: @producer)
+      @juliann_p   = FactoryGirl.create(:user_role, user: @juliann, role:  @producer)
     end
     
     def destroy_user_producer_roles
-      @bibby_p.delete
-      @dean_p.delete
+      @ev_p.delete
+      @juliann_p.delete
     end
 
     def create_user_mixer_roles
-      @mike_p  = FactoryGirl.create(:user_role, user: @jim, role: @mixer)
+      @jim_p  = FactoryGirl.create(:user_role, user: @jim, role: @mixer)
     end
     
     def destroy_user_mixer_roles
-      @mike_p.delete
+      @jim_p.delete
     end
 
   end
