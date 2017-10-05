89cd395939234c1f59362943b040ffdf1d52ea12
Juliann <jbfields@andrew.cmu.edu>
2017-10-05 11:05:27 -0400

    project controller params correction

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,9 @@
 class ProjectsController < ApplicationController
+    # before_action :check_login, except [:index, :show]   
+    before_action :set_project, only: [:show, :edit, :update, :destroy]
 
     def index
-        @active_peojects = Project.active.chronological.alphabetical.paginate(page: params[:page]).per_page(10)
+        @active_projects = Project.active.chronological.alphabetical.paginate(page: params[:page]).per_page(10)
         @completed_projects = Project.completed.chronological.alphabetical.paginate(page: params[:page]).per_page(10)
         @all_projects = Project.chronological.alphabetical.paginate(page: params[:page]).per_page(10)
     end
@@ -12,8 +14,6 @@ class ProjectsController < ApplicationController
 
     def new
         @project = Project.new
-        @project.start_date = Date.today
-        @project.owner = current_user.id
     end
 
     def edit
@@ -21,21 +21,23 @@ class ProjectsController < ApplicationController
 
     def create
         @project = Project.new(project_params)
-        @project.owner = current_user.id  # set the owner to the user who created the project
+        @project.start_date = Date.today
+        @project.owner = @current_user  # set the owner to the user who created the project
     
         if @project.save
             # if saved to database
-            flash[:notice] = "#{@project.name} has been created."
+            flash[:notice] = "#{@project.title} has been created."
             redirect_to @project # go to show project page
         else
             # return to the 'new' form
+			flash[:error] = "This project could not be created."
             render :action => 'new'
         end
     end
 
     def update
         if @project.update(project_params)
-            flash[:notice] = "#{@project.name} has been updated."
+            flash[:notice] = "#{@project.title} has been updated."
             redirect_to @project
         else
             render :action => 'edit'
@@ -43,11 +45,19 @@ class ProjectsController < ApplicationController
     end
 
     def destroy
+        @project.destroy
+        flash[:notice] = "Successfully removed #{@project.title}."
+        redirect_to projects_url        
     end
 
     private
-        def project_params
-            params.require(:project).permit(:owner_id, :proj_description, :genre, :title, :start_date, :end_date, :category, :preview_level)
-        end
+
+    def set_project
+        @project = Project.find(params[:id])
+    end
+
+    def project_params
+        params.fetch(:project, {}).permit(:proj_description, :genre, :title, :category, :preview_level, :status)
+    end
 
 end
